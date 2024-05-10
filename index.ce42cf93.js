0c726ca54ee19636a9013dde69f0f79ea555e9cb
Kerb <hakkerby01@gmail.com>
2024-05-10 10:37:42 +0700

    ADDED DIST FOLDER

---
+++ index.ce42cf93.js
@@ -0,0 +1,10 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+for(i = 0; i < coll.length; i++)coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) content.style.maxHeight = null;
+    else content.style.maxHeight = content.scrollHeight + "px";
+});
+
+//# sourceMappingURL=index.ce42cf93.js.map
